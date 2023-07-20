d9f559205ab31296eee0214601776efe54f8d319
Vivek Chowdary <vivekchowdary@Viveks-MacBook-Air.local>
2023-07-21 00:40:38 +0530

    clean up

--- components/Orders/OrderItem.jsx
+++ components/Orders/OrderItem.jsx
@@ -40,20 +40,5 @@ export default function OrderItem({ order, date, id, onDelete }) {
         </button>
       </div>
     </div>
-    // <li className="border border-gray-100 text-white rounded-md bg-gray-900 px-6 pt-6 pb-3">
-    //   <div className="flex justify-between items-center">
-    //     <div>
-    //       <h1 className="pb-3 font-lunasima">{name}</h1>
-    //       <h3 className="text-cyan-500">Total Price: ₹{price}</h3>
-    //     </div>
-    //     <div>
-    //       <h2 className="pb-3">Order Placed</h2>
-    //       <span className="">{date}</span>
-    //     </div>
-    //   </div>
-    //   <div className="mt-4">
-    //     <button onClick={handleDelete} className="black_btn mx-auto">Delete</button>
-    //   </div>
-    // </li>
   );
 }
