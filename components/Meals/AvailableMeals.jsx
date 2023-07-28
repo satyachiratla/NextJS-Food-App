c2939bd5000fd323edbb06db4e3628cf27178f51
Vivek Chowdary <vivekchowdary@Viveks-MacBook-Air.local>
2023-07-28 14:52:02 +0530

    cleanup

--- components/Meals/AvailableMeals.jsx
+++ components/Meals/AvailableMeals.jsx
@@ -3,7 +3,7 @@ import Meals from "./Meals";
 export default function AvailableMeals() {
   return (
     <div className="mt-10 md:mt-16 mx-auto">
-      <h1 className=" text-center font-hind font-bold text-amber-700 text-[1.8rem] tracking-wide md:text-3xl">
+      <h1 className="text-center font-hind font-bold text-amber-600 text-[1.8rem] tracking-wide md:text-3xl">
         Available Meals
       </h1>
       <Meals />
--- components/Meals/MealsSummary.jsx
+++ components/Meals/MealsSummary.jsx
@@ -1,7 +1,7 @@
 export default function MealsSummary() {
   return (
     <section className="border border-gray-200 rounded-xl p-8 bg-slate-100 max-w-sm md:max-w-xl mx-auto my-8 text-center space-y-6">
-      <h2 className="font-semibold text-xl tracking-wide blue_gradient md:text-2xl">Delicious Food, Delivered To You</h2>
+      <h2 className="font-semibold text-center text-xl tracking-wide text-[#006600] md:text-2xl">Delicious Food, Delivered To You</h2>
       <p className="text-base tracking-wide leading-6 md:text-lg">
         Choose your favorite meal from our broad selection of available meals
         and enjoy a delicious lunch or dinner at home.
